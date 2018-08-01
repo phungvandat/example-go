213940cc5ef1f4e7a1359b3a1aa2aa8c14973215
phungvandat <phungvandat97@gmail.com>
2018-08-01 14:01:43 +0700

    CRUD category a book: - Create file category.go into package domain. - Create migration. - Create service for category. - create endpoint for category. - Add routers in http/http.go. - Add service in cmd/server/main.go.

---
+++ service/category/error.go
@@ -0,0 +1,39 @@
+package category
+
+import (
+	"net/http"
+)
+
+// Error Declaration
+var (
+	ErrNotFound       = errNotFound{}
+	ErrUnknown        = errUnknown{}
+	ErrRecordNotFound = errRecordNotFound{}
+)
+
+type errNotFound struct{}
+
+func (errNotFound) Error() string {
+	return "record not found"
+}
+func (errNotFound) StatusCode() int {
+	return http.StatusNotFound
+}
+
+type errUnknown struct{}
+
+func (errUnknown) Error() string {
+	return "unknown error"
+}
+func (errUnknown) StatusCode() int {
+	return http.StatusBadRequest
+}
+
+type errRecordNotFound struct{}
+
+func (errRecordNotFound) Error() string {
+	return "client record not found"
+}
+func (errRecordNotFound) StatusCode() int {
+	return http.StatusNotFound
+}
